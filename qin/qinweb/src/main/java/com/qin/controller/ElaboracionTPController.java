bd68f3f83cf16ffcaa53a54ee6713fae7f90cf79
moreyramj <moreyramj@7ec1fd7f-e379-37a6-ff35-e735af43d7cd>
2011-04-21 18:41:27 +0000

    arregle layout, agregue tiles, y ya tenemos un mini ejemplo funcional de guardado de tp

--- qin/qinweb/src/main/java/com/qin/controller/ElaboracionTPController.java
+++ qin/qinweb/src/main/java/com/qin/controller/ElaboracionTPController.java
@@ -1,14 +1,65 @@
 package com.qin.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
+import com.qin.entity.Enunciado;
+import com.qin.entity.ItemProductoAcademico;
+import com.qin.entity.Materia;
+import com.qin.entity.TrabajoPractico;
+import com.qin.manager.colaboracion.ColaboracionManager;
+
 @Controller
-public class ElaboracionTPController extends MultiActionController{
+public class ElaboracionTPController extends MultiActionController {
+
+	@Autowired
+	private ColaboracionManager colaboracionManager;
 
 	@RequestMapping(value = "/alta_tp.html")
-	public String altaTP(){
-		return "/trabajos_practicos/alta_tp";
+	public String altaTP() {
+		return "tp.alta";
 	}
+
+	@RequestMapping(value = "/guardar_tp.html")
+	public String guardarTP(@RequestParam("contenido") String contenido) throws Exception {
+
+		Materia materia = new Materia();
+		materia.setId(null);
+		materia.setAnio(new Long(2011));
+		materia.setCarrera("Ingeniería en Informática");
+		materia.setCuatrimestre(1);
+		materia.setCodigo("7565");
+		materia.setNombre("75.65 - Manufactura Integrada por Computadora (CIM) I");
+
+		Enunciado punto1 = new Enunciado();
+		punto1.setId(null);
+		punto1.setPuntos(new Double(10));
+		punto1.setEnunciado(contenido);
+
+		List<ItemProductoAcademico> itemProductoAcademicos = new ArrayList<ItemProductoAcademico>();
+		itemProductoAcademicos.add(punto1);
+		TrabajoPractico trabajoPractico = new TrabajoPractico();
+		trabajoPractico.setId(null);
+		trabajoPractico.setDictamen(null);
+		trabajoPractico.setMateria(materia);
+		trabajoPractico.setItemProductoAcademicos(itemProductoAcademicos);
+		colaboracionManager.insertTP(trabajoPractico);
+
+		return "tp.alta";
+	}
+
+	public void setColaboracionManager(ColaboracionManager colaboracionManager) {
+		this.colaboracionManager = colaboracionManager;
+	}
+
+	public ColaboracionManager getColaboracionManager() {
+		return colaboracionManager;
+	}
+
 }
