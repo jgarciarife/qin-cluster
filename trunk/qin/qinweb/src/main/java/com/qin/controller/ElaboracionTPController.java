8373dd328225083b5a7cdf2d522d001f30e1572a
moreyramj <moreyramj@7ec1fd7f-e379-37a6-ff35-e735af43d7cd>
2011-04-25 01:45:17 +0000

    spring mvc a full

--- trunk/qin/qinweb/src/main/java/com/qin/controller/ElaboracionTPController.java
+++ trunk/qin/qinweb/src/main/java/com/qin/controller/ElaboracionTPController.java
@@ -3,13 +3,14 @@ package com.qin.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.qin.entity.Enunciado;
 import com.qin.entity.ItemProductoAcademico;
@@ -18,45 +19,41 @@ import com.qin.entity.TrabajoPractico;
 import com.qin.manager.colaboracion.ColaboracionManager;
 
 @Controller
-public class ElaboracionTPController extends MultiActionController {
+public class ElaboracionTPController {
+	protected static Logger logger = LoggerFactory
+			.getLogger(ElaboracionTPController.class);
 
 	@Autowired
 	private ColaboracionManager colaboracionManager;
 
 	@ModelAttribute("materias")
 	public List<Materia> popularMaterias() throws Exception {
-		return  colaboracionManager.findAllMaterias();
+		return colaboracionManager.findAllMaterias();
+	}
+
+	@ModelAttribute("trabajoPractico")
+	public TrabajoPractico newRequest() {
+		TrabajoPractico trabajoPractico = new TrabajoPractico();
+		List<ItemProductoAcademico> itemProductoAcademicos = new ArrayList<ItemProductoAcademico>();
+		Enunciado punto1 = new Enunciado();
+		punto1.setId(null);
+		punto1.setPuntos(new Double(10));
+		itemProductoAcademicos.add(punto1);
+		trabajoPractico.setItemProductoAcademicos(itemProductoAcademicos);
+		return trabajoPractico;
 	}
 
 	@RequestMapping(value = "/alta_tp.html")
 	public String altaTP(Model model) throws Exception {
-		List<Materia> materias = colaboracionManager.findAllMaterias();
-		model.addAttribute("materia", materias.get(0).getCodigo());
 		return "tp.alta";
 	}
 
-	@RequestMapping(value = "/guardar_tp.html")
-	public String guardarTP(@RequestParam("contenido") String contenido) throws Exception {
-		Materia materia = new Materia();
-		materia.setId(null);
-		materia.setAnio(new Long(2011));
-		materia.setCarrera("Ingeniería en Informática");
-		materia.setCuatrimestre(1);
-		materia.setCodigo("7565");
-		materia.setNombre("75.65 - Manufactura Integrada por Computadora (CIM) I");
-
-		Enunciado punto1 = new Enunciado();
-		punto1.setId(null);
-		punto1.setPuntos(new Double(10));
-		punto1.setEnunciado(contenido);
-
-		List<ItemProductoAcademico> itemProductoAcademicos = new ArrayList<ItemProductoAcademico>();
-		itemProductoAcademicos.add(punto1);
-		TrabajoPractico trabajoPractico = new TrabajoPractico();
-		trabajoPractico.setId(null);
-		trabajoPractico.setDictamen(null);
-		trabajoPractico.setMateria(materia);
-		trabajoPractico.setItemProductoAcademicos(itemProductoAcademicos);
+	@RequestMapping(value = "/guardar_tp.html", method = RequestMethod.POST)
+	public String guardarTP(TrabajoPractico trabajoPractico) throws Exception {
+		logger.info("materia " + trabajoPractico.getMateria().getId());
+		logger.info("enunciado "
+				+ ((Enunciado) trabajoPractico.getItemProductoAcademicos().get(
+						0)).getEnunciado());
 		colaboracionManager.insertTP(trabajoPractico);
 
 		return "tp.alta";
