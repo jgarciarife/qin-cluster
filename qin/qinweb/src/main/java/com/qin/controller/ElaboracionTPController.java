47cc2d02ed0b3f551393d1c2c2f619add2914356
moreyramj <moreyramj@gmail.com>
2011-04-28 01:58:59 +0000

    update del tp y busqueda de tps

--- qin/qinweb/src/main/java/com/qin/controller/ElaboracionTPController.java
+++ qin/qinweb/src/main/java/com/qin/controller/ElaboracionTPController.java
@@ -11,12 +11,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.qin.entity.Enunciado;
 import com.qin.entity.ItemProductoAcademico;
 import com.qin.entity.Materia;
 import com.qin.entity.TrabajoPractico;
 import com.qin.manager.colaboracion.ColaboracionManager;
+import com.qin.manager.trabajoPractico.TrabajoPracticoManager;
 
 @Controller
 public class ElaboracionTPController {
@@ -26,20 +28,36 @@ public class ElaboracionTPController {
 	@Autowired
 	private ColaboracionManager colaboracionManager;
 
+	@Autowired
+	private TrabajoPracticoManager trabajoPracticoManager;
+
 	@ModelAttribute("materias")
 	public List<Materia> popularMaterias() throws Exception {
 		return colaboracionManager.findAllMaterias();
 	}
 
 	@ModelAttribute("trabajoPractico")
-	public TrabajoPractico newRequest() {
-		TrabajoPractico trabajoPractico = new TrabajoPractico();
-		List<ItemProductoAcademico> itemProductoAcademicos = new ArrayList<ItemProductoAcademico>();
-		Enunciado punto1 = new Enunciado();
-		punto1.setId(null);
-		punto1.setPuntos(new Double(10));
-		itemProductoAcademicos.add(punto1);
-		trabajoPractico.setItemProductoAcademicos(itemProductoAcademicos);
+	public TrabajoPractico newRequest(@RequestParam(required = false) Long id) {
+		TrabajoPractico trabajoPractico = null;
+		if (id != null) {
+			try {
+				logger.info("id " + id);
+				trabajoPractico = trabajoPracticoManager.findById(id);
+			} catch (Exception e) {
+				logger.error("error al buscar", e);
+			}
+
+		}
+		if (trabajoPractico == null) {
+			logger.info("creando un nuevo TP");
+			trabajoPractico = new TrabajoPractico();
+			List<ItemProductoAcademico> itemProductoAcademicos = new ArrayList<ItemProductoAcademico>();
+			Enunciado punto1 = new Enunciado();
+			punto1.setId(null);
+			punto1.setPuntos(new Double(10));
+			itemProductoAcademicos.add(punto1);
+			trabajoPractico.setItemProductoAcademicos(itemProductoAcademicos);
+		}
 		return trabajoPractico;
 	}
 
@@ -49,13 +67,18 @@ public class ElaboracionTPController {
 	}
 
 	@RequestMapping(value = "/guardar_tp.html", method = RequestMethod.POST)
-	public String guardarTP(TrabajoPractico trabajoPractico) throws Exception {
+	public String guardarTP(TrabajoPractico trabajoPractico, Model model)
+			throws Exception {
 		logger.info("materia " + trabajoPractico.getMateria().getId());
 		logger.info("enunciado "
 				+ ((Enunciado) trabajoPractico.getItemProductoAcademicos().get(
 						0)).getEnunciado());
-		colaboracionManager.insertTP(trabajoPractico);
-
+		if (trabajoPractico.getId() == null) {
+			colaboracionManager.insertTP(trabajoPractico);
+		} else {
+			colaboracionManager.updateTP(trabajoPractico);
+		}
+		model.addAttribute("id", trabajoPractico.getId());
 		return "tp.alta";
 	}
 
@@ -65,6 +88,15 @@ public class ElaboracionTPController {
 
 	public ColaboracionManager getColaboracionManager() {
 		return colaboracionManager;
+	}
+
+	public void setTrabajoPracticoManager(
+			TrabajoPracticoManager trabajoPracticoManager) {
+		this.trabajoPracticoManager = trabajoPracticoManager;
+	}
+
+	public TrabajoPracticoManager getTrabajoPracticoManager() {
+		return trabajoPracticoManager;
 	}
 
 }
--- qin/qinweb/src/main/webapp/WEB-INF/jsp/trabajos_practicos/resultado_busqueda_tps.jsp
+++ qin/qinweb/src/main/webapp/WEB-INF/jsp/trabajos_practicos/resultado_busqueda_tps.jsp
@@ -1,1 +1,8 @@
-asd2
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib uri="http://displaytag.sf.net" prefix="display"%>
+
+
+<display:table name="trabajos" id="row" >
+  <display:column property="titulo" title="Titulo"/>
+</display:table>
