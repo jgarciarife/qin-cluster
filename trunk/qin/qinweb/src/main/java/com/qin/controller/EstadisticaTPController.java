2fd98a51c12a612b515039884576918e1f5549e4
moreyramj <moreyramj@7ec1fd7f-e379-37a6-ff35-e735af43d7cd>
2011-05-04 01:36:57 +0000

    Estadisticas

---
+++ trunk/qin/qinweb/src/main/java/com/qin/controller/EstadisticaTPController.java
@@ -0,0 +1,47 @@
+package com.qin.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.qin.entity.Materia;
+import com.qin.manager.colaboracion.ColaboracionManager;
+
+@Controller
+public class EstadisticaTPController {
+	protected static Logger logger = LoggerFactory
+			.getLogger(EstadisticaTPController.class);
+
+	@Autowired
+	private ColaboracionManager colaboracionManager;
+
+	@ModelAttribute("materias")
+	public List<Materia> popularMaterias() throws Exception {
+		return getColaboracionManager().findAllMaterias();
+	}
+
+	@RequestMapping(value = "/estadistica_tp.html")
+	public String altaTP(Model model) throws Exception {
+		return "tp.estadistica";
+	}
+	
+	@RequestMapping(value = "/ver_estadistica.html")
+	protected String buscarTPs(Long materiaId) throws Exception {
+		return "tp.ver_estadistica_tp_en_materia";
+	}
+
+	public void setColaboracionManager(ColaboracionManager colaboracionManager) {
+		this.colaboracionManager = colaboracionManager;
+	}
+
+	public ColaboracionManager getColaboracionManager() {
+		return colaboracionManager;
+	}
+
+}
--- trunk/qin/qinweb/src/main/webapp/WEB-INF/jsp/layout/menu.jsp
+++ trunk/qin/qinweb/src/main/webapp/WEB-INF/jsp/layout/menu.jsp
@@ -2,3 +2,4 @@
 <span><a href="iniciar_busqueda.html">Buscar</a></span>
 <span><a href="consultar.html">Consultar</a></span>
 <span><a href="alta_tp.html">Alta TP</a></span>
+<span><a href="estadistica_tp.html">Estadistica TP</a></span>
