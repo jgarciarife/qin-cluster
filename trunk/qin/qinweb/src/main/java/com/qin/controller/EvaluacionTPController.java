2c633d60e13edf1273f34f8ecd9be10048aa8b75
moreyramj@gmail.com <moreyramj@gmail.com@7ec1fd7f-e379-37a6-ff35-e735af43d7cd>
2011-05-21 21:54:28 +0000

    dictamen!

--- trunk/qin/qinweb/src/main/java/com/qin/controller/EvaluacionTPController.java
+++ trunk/qin/qinweb/src/main/java/com/qin/controller/EvaluacionTPController.java
@@ -7,8 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.qin.entity.Correccion;
+import com.qin.entity.Dictamen;
 import com.qin.entity.Resolucion;
+import com.qin.manager.dictamen.DictamenManager;
 import com.qin.manager.resolucion.ResolucionManager;
 
 @Controller
@@ -19,16 +23,57 @@ public class EvaluacionTPController {
 	@Autowired
 	private ResolucionManager resolucionManager;
 
+	@Autowired
+	private DictamenManager dictamenManager;
+
 	@RequestMapping(value = "/evaluar_resolucion.html")
-	public String evaluarResol(Long id, Model model) throws Exception {
+	public String evaluarResol(
+			@RequestParam(value = "id", required = false) Long id, Model model)
+			throws Exception {
 		Resolucion res = resolucionManager.findById(id);
 		model.addAttribute("resolucion", res);
+
+		Dictamen dictamen = null;
+		if (id != null) {
+			try {
+				logger.info("id " + id);
+				dictamen = getDictamenManager().findById(id);
+			} catch (Exception e) {
+				logger.error("error al buscar", e);
+			}
+
+		}
+
+		if (dictamen == null) {
+			dictamen = new Dictamen();
+		}
+
+		model.addAttribute("dictamen", dictamen);
 		return "resolucion.evaluacion";
 	}
 
 	@RequestMapping(value = "/guardar_evaluacion_resolucion.html", method = RequestMethod.POST)
-	public String guardarEvaluacionResolucion(Resolucion resolucion,
-			Model model) throws Exception {
+	public String guardarEvaluacionResolucion(Dictamen dictamen, Model model)
+			throws Exception {
+		Double total = 0D;
+		if (dictamen.getCorreccions() != null) {
+			for (Correccion r : dictamen.getCorreccions()) {
+				r.setDictamen(dictamen);
+				total += r.getPuntaje();
+			}
+		}
+		dictamen.setPuntaje(total);
+		if (dictamen.getId() == null) {
+			dictamenManager.insert(dictamen);
+		} else {
+			dictamenManager.update(dictamen);
+		}
+
+		dictamen = dictamenManager.findById(dictamen.getId());
+		model.addAttribute("dictamen", dictamen);
+		Resolucion res = resolucionManager.findById(dictamen.getResolucion()
+				.getId());
+		model.addAttribute("resolucion", res);
 		return "resolucion.evaluacion";
 	}
 
@@ -38,6 +83,14 @@ public class EvaluacionTPController {
 
 	public ResolucionManager getResolucionManager() {
 		return resolucionManager;
+	}
+
+	public void setDictamenManager(DictamenManager dictamenManager) {
+		this.dictamenManager = dictamenManager;
+	}
+
+	public DictamenManager getDictamenManager() {
+		return dictamenManager;
 	}
 
 }
